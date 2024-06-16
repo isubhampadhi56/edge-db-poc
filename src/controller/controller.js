5e9fb2710ae60b80b0fbaa5446e7a3444c627d2d
Subham <subham@isubhampadhi.com>
2024-06-16 14:58:30 +0530

    Fixed Prisma Issue in controller

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -1,14 +1,19 @@
-const { prisma } = require('../config/sqlConn.js');
+const prisma =  require('../config/sqlConn.js');
+
 async function addProduct(req,res) {
     const createProduct = await prisma.product.create({
         data: {
-            storeId: req.storeId,
-            categoryId: req.categoryId,
-            supplierId: req.supplierId,
-            productName: req.productName,
-            quantity: req.quantity,
-            price: req.price,
-            description: req.description
+            supplierId: req.body.supplierId,
+            productName: req.body.productName,
+            quantity: req.body.quantity,
+            price: req.body.price,
+            description: req.body.description,
+            storeRel: {
+                connect: { storeId: req.body.storeId } // This will throw an error if the store with the given id does not exist
+            },
+            categoryRel: {
+                connect: { categoryId: req.body.categoryId } // This will throw an error if the store with the given id does not exist
+            }
         }
     });
     // if(createProduct.) {
@@ -18,9 +23,9 @@ async function addProduct(req,res) {
 async function addStore(req,res){
     const createStore = await prisma.store.create({
         data:{
-            storeName: req.storeName,
-            address: req.address,
-            description:req.description
+            storeName: req.body.storeName,
+            address: req.body.address,
+            description:req.body.description
         }
     });
     res.json(createStore);
@@ -29,21 +34,21 @@ async function addStore(req,res){
 async function addCategory(req,res){
     const createCategory = await prisma.category.create({
         data: {
-            storeId: req.storeId,
-            categoryId: req.categoryId,
-            description: req.description
+            storeId: req.body.storeId,
+            categoryName: req.body.categoryName,
+            description: req.body.description
         }
     });
     res.json(createCategory);
 }
 
 async function addUser(req, res) {
-    const createUser = await prisma.users.create({
+    const createUser = await prisma.user.create({
         data:{
-            storeId: req.storeId,
-            username: req.username,
-            password: req.password,
-            role: req.role
+            storeId: req.body.storeId,
+            username: req.body.username,
+            password: req.body.password,
+            role: req.body.role
         }
     });
     res.json(createUser);
